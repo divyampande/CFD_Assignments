5733e9f94ffc033f37edc4921459e8e18b702efc
Divyam <divyampandeyy@outlook.com>
2026-03-27 13:27:47 +0530

    Added the data export function.

--- Assignment_2/main.f90
+++ Assignment_2/main.f90
@@ -71,13 +71,16 @@ program main
     best_omega_psor = 1.0_wp
     best_c_time_psor = 0.0_wp
     
-    do w_int = 1, 19
-        omega = real(w_int, wp) / 10.0_wp
+    ! Since I already ran between 1 to 1.9 with a step of 0.001,
+    ! I already know the optimal omega is around 1.835, so I will just sweep,
+    ! between 1.8 and 1.85 to save time. If I had to do the entire range, I would just change the limits of this loop.
+    do w_int = 1800, 1850
+        omega = real(w_int, wp) / 1000.0_wp
         call reset_grid(T, imax, jmax)
         call solve_PSOR(T, imax, jmax, dx, dy, omega, iters, c_time)
         
         ! Print every test to the table
-        write(*, '(A10, I20, F20.6, F15.2)') "PSOR", iters, c_time, omega
+        write(*, '(A10, I20, F20.6, F15.3)') "PSOR", iters, c_time, omega
         
         ! Save the best one
         if (iters < min_iters_psor) then
@@ -87,19 +90,22 @@ program main
         end if
     end do
     print *, ">> OPTIMAL PSOR: Omega = ", best_omega_psor, min_iters_psor, " iters"
-    write(csv_id, '(A10, I20, F20.6, F15.2)') "PSOR", min_iters_psor, best_c_time_psor, best_omega_psor
+    write(csv_id, '(A10, I20, F20.6, F15.3)') "PSOR", min_iters_psor, best_c_time_psor, best_omega_psor
 
     ! Optimize LSOR
     min_iters_lsor = 999999
     best_omega_lsor = 1.0_wp
     best_c_time_lsor = 0.0_wp
     
-    do w_int = 1, 19
-        omega = real(w_int, wp) / 10.0_wp
+    ! Since I already ran between 1 to 1.9 with a step of 0.001,
+    ! I already know the optimal omega is around 1.777, so I will just sweep,
+    ! between 1.75 and 1.8 to save time. If I had to do the entire range, I would just change the limits of this loop.
+    do w_int = 1750, 1800
+        omega = real(w_int, wp) / 1000.0_wp
         call reset_grid(T, imax, jmax)
         call solve_LSOR(T, imax, jmax, dx, dy, omega, iters, c_time)
         
-        write(*, '(A10, I20, F20.6, F15.2)') "LSOR", iters, c_time, omega
+        write(*, '(A10, I20, F20.6, F15.3)') "LSOR", iters, c_time, omega
         
         if (iters < min_iters_lsor) then
             min_iters_lsor = iters
@@ -108,9 +114,16 @@ program main
         end if
     end do
     print *, ">> OPTIMAL LSOR: Omega = ", best_omega_lsor, min_iters_lsor, " iters"
-    write(csv_id, '(A10, I20, F20.6, F15.2)') "LSOR", min_iters_lsor, best_c_time_lsor, best_omega_lsor
+    write(csv_id, '(A10, I20, F20.6, F15.3)') "LSOR", min_iters_lsor, best_c_time_lsor, best_omega_lsor
 
     close(csv_id)
+
+    ! DATA EXPORT
+    ! We will use LSOR to generate the final steady-state field since all 
+    ! methods converge to the exact same physical answer.
+    call reset_grid(T, imax, jmax)
+    call solve_LSOR(T, imax, jmax, dx, dy, best_omega_lsor, iters, c_time)
+    call export_to_csv("results/prob1_results.csv", T, imax, jmax, dx, dy)
 
 ! INTERNAL SUBROUTINES
 contains
@@ -127,4 +140,28 @@ contains
         T_array(max_i, :) = 0.0_wp     ! Right
     end subroutine reset_grid
 
+    ! Writes the 2D grid into a 1D X, Y, T format for Python
+    subroutine export_to_csv(filename, T_array, max_i, max_j, d_x, d_y)
+        character(len=*), intent(in) :: filename
+        real(wp), intent(in)         :: T_array(:,:), d_x, d_y
+        integer, intent(in)          :: max_i, max_j
+        
+        integer  :: i, j
+        real(wp) :: x_pos, y_pos
+
+        open(newunit=csv_id, file=filename, status='replace')
+        write(csv_id, '(A)') "X,Y,T"
+        
+        do j = 1, max_j
+            y_pos = real(j - 1, wp) * d_y
+            do i = 1, max_i
+                x_pos = real(i - 1, wp) * d_x
+                ! Dump coordinates and temperature
+                write(csv_id, '(F10.6, A, F10.6, A, F10.6)') x_pos, ",", y_pos, ",", T_array(i,j)
+            end do
+        end do
+        
+        close(csv_id)
+        print *, ">> Steady-state data exported to: ", filename
+    end subroutine export_to_csv
 end program main
